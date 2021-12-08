71dbc049a3fa5c6e49551bd071aaa3e420bec73b
Eddie Zaneski <eddiezane@gmail.com>
2021-12-07 17:58:48 -0700

    Add day 3 part 1
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- day-3/main.go
+++ day-3/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -23,7 +25,46 @@ func stepone() {
 
 	s := bufio.NewScanner(f)
 
+	input := make([]string, 0)
 	for s.Scan() {
-		l := s.Text()
+		input = append(input, s.Text())
 	}
+
+	bitLength := len(input[0])
+	sums := make([]int, bitLength)
+
+	for _, v := range input {
+		bits := strings.Split(v, "")
+		for ix, iv := range bits {
+			bit, err := strconv.Atoi(iv)
+			if err != nil {
+				log.Fatal("error parsing bit:", err)
+			}
+			sums[ix] += bit
+		}
+	}
+
+	gamma := ""
+	eps := ""
+	for _, v := range sums {
+		if float64(v) > (float64(len(input)) / 2) {
+			gamma = gamma + "1"
+			eps = eps + "0"
+		} else {
+			gamma = gamma + "0"
+			eps = eps + "1"
+		}
+	}
+
+	gi, err := strconv.ParseUint(gamma, 2, len(sums))
+	if err != nil {
+		log.Fatal("error parsing gamma:", err)
+	}
+
+	ei, err := strconv.ParseUint(eps, 2, len(sums))
+	if err != nil {
+		log.Fatal("error parsing epsilon:", err)
+	}
+
+	log.Println(gi * ei)
 }
