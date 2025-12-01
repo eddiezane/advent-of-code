e0435ff62174d12e27e86d66cf645fa5b1c98106
Eddie Zaneski <eddiezane@gmail.com>
2025-12-01 09:28:08 -0700

    day 1 part 1

---
+++ 2025/rust/src/day-1/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let input = include_str!("../../../inputs/day-1/input.txt");
+
+    let mut pos: i32 = 50;
+    let mut count: i32 = 0;
+
+    for line in input.lines() {
+        let (dir, rest) = line.split_at(1);
+
+        let mut delta = rest.parse::<i32>().unwrap();
+
+        if dir == "L" {
+            delta = -delta;
+        }
+
+        pos = (pos + delta).rem_euclid(100);
+        if pos == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{count}");
+}
