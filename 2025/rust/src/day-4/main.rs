cec8701ca094771e42e1ce5ad4160ae01bf973fd
Eddie Zaneski <eddiezane@gmail.com>
2025-12-09 16:06:34 -0500

    day 4 part 2
    Signed-off-by: Eddie Zaneski <eddiezane@gmail.com>

--- 2025/rust/src/day-4/main.rs
+++ 2025/rust/src/day-4/main.rs
@@ -1,17 +1,4 @@
-use std::fmt;
-
-type Grid = Vec<Vec<String>>;
-
-struct DisplayGrid<'a>(&'a Grid);
-
-impl<'a> fmt::Display for DisplayGrid<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in self.0 {
-            writeln!(f, "{}", row.join(""))?;
-        }
-        Ok(())
-    }
-}
+type Grid = Vec<Vec<char>>;
 
 const DELTAS: &[(i32, i32)] = &[
     (0, -1),
@@ -27,42 +14,51 @@ const DELTAS: &[(i32, i32)] = &[
 fn main() {
     let input = include_str!("../../../inputs/day-4/input.txt");
 
-    let grid: Grid = input
-        .lines()
-        .filter(|l| !l.is_empty())
-        .map(|l| l.trim().chars().map(String::from).collect())
-        .collect();
-
-    // println!("{}", DisplayGrid(&grid));
+    let mut grid: Grid = input.lines().map(|l| l.trim().chars().collect()).collect();
 
     let mut accessible = 0_i32;
 
-    for (y, row) in grid.iter().enumerate() {
-        // println!("row");
-        for (x, cell) in row.iter().enumerate() {
-            // println!("cell");
-            let mut adj = 0_i32;
-            // println!("{x},{y},{cell}\n===");
-            for &(dx, dy) in DELTAS {
-                let ndx = x as i32 + dx;
-                let ndy = y as i32 + dy;
-                // println!("{ndx},{ndy}");
-                if ndx >= 0 && ndy >= 0 && ndx < row.len() as i32 && ndy < grid.len() as i32 {
-                    let c = &grid[ndy as usize][ndx as usize];
-                    // println!("adj: {c}");
-                    if c == "@" {
-                        adj += 1;
-                    }
+    let height = grid.len() as i32;
+    let width = grid[0].len() as i32;
+
+    let mut to_update: Vec<(usize, usize)> = Vec::new();
+
+    loop {
+        to_update.clear();
+        for (y, row) in grid.iter().enumerate() {
+            for (x, &cell) in row.iter().enumerate() {
+                if cell != '@' {
+                    continue;
+                }
+
+                let adj = DELTAS
+                    .iter()
+                    .filter(|&&(dx, dy)| {
+                        let nx = x as i32 + dx;
+                        let ny = y as i32 + dy;
+
+                        if nx >= 0 && nx < width && ny >= 0 && ny < height {
+                            grid[ny as usize][nx as usize] == '@'
+                        } else {
+                            false
+                        }
+                    })
+                    .count();
+
+                if adj < 4 {
+                    to_update.push((x, y));
                 }
             }
-            // println!();
-            if cell == "@" && adj < 4 {
-                // println!("accessible");
-                accessible += 1;
-            }
-            // println!("end col");
         }
-        // println!("end row\n\n\n");
+
+        if to_update.is_empty() {
+            break;
+        }
+
+        for (x, y) in &to_update {
+            accessible += 1;
+            grid[*y][*x] = 'X';
+        }
     }
     println!("{accessible}");
 }
